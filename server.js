b87bd5b5097cda32dc98377b1e0ac2c2472f7d47
Evan Coakley <evancoakley1@gmail.com>
2017-09-05 15:03:28 -0500

    complete

--- server.js
+++ server.js
@@ -1,30 +1,56 @@
-const express = require('express');
-const mustacheExpress = require('mustache-express');
-const busBoy = require("busboy");
+const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 8000;
+const expressValidator = require("express-validator");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
 
-var user;
+const port = process.env.PORT || 8080;
+const app = express();
+
 
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "./public")));
 app.set("view engine", "mustache");
 
-app.use(express.static(path.join(__dirname, "./public")));
-app.use(bodyParser.urlencoded({
-    extended: true
 
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const todoList = [];
+
+app.get("/", (req, res) => {
+
+    const completeTodos = [];
+    const incompleteTodos = [];
+
+    todoList.forEach(function (todo) {
+        if (todo.completed) {
+            completeTodos.push(todo);
+        } else {
+            incompleteTodos.push(todo);
+        }
+    });
 
 
-
-
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    res.render("todo", { complete: completeTodos, incomplete: incompleteTodos })
 });
 
 
+app.post("/", function (req, res) {
+    let newTodo = req.body;
+    newTodo.completed = false;
+    newTodo.id = Math.random();
+    todoList.push(newTodo);
+    return res.redirect('/')
+})
+
+app.post("/todos/:id", function (req, res) {
+    let id = req.params.id;
+    let todo = todoList.find(todo => todo.id === parseFloat(id));
+    todo.completed = !todo.completed;
+    return res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}!`);
+});
